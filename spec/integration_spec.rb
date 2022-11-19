195d373944451d60a7ac22207880eacdefbbe80f
Marina Papapetrou <contact@marinapapap.com>
2022-11-19 12:08:35 +0000

    passed first integrated test

--- spec/integration_spec.rb
+++ spec/integration_spec.rb
@@ -4,7 +4,7 @@ require 'dish_list'
 RSpec.describe "integration" do
 
   context "when dishes are added to the dish list" do
-    xit "returns full list" do
+    it "returns full list" do
       dish_list = DishList.new
       dish_1 = Dish.new("pizza", 12.00)
       dish_2 = Dish.new("pasta", 12.00)
