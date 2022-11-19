357032ab34d117f6d9c937b34eff889495212748
Marina Papapetrou <contact@marinapapap.com>
2022-11-19 10:37:29 +0000

    file setup

---
+++ lib/dish.rb
@@ -0,0 +1,26 @@
+class Dish
+  def initialize(dish, price) # dish is a string and price is float
+  end
+
+  def dish # returns dish as string
+  end
+
+  def price # returns price as float
+  end
+
+  def format
+    # Returns a string of the form "dish, £price"
+  end
+
+  def select
+    # formats dish for selection
+  end
+
+  def deselect
+    # reverse formatting, deselects dish
+  end
+
+  def is_selected?
+    # returns true if dish has been selected
+  end
+end
---
+++ lib/dish_list.rb
@@ -0,0 +1,22 @@
+class DishList
+  def initialize
+    # ...
+  end
+
+  def add(dish_with_price) # dish_with_price is an instance of Dish
+    # Dish gets added to the menu
+    # Returns nothing
+  end
+
+  def list
+    # Returns list of dishes
+  end
+  
+  def selection
+    # returns selected dishes
+  end
+
+  def receipt(selection)
+  # takes selection and returns itemised receipt with grand total
+  end
+end
